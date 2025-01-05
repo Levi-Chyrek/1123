278c22c0791afa37cef03dcd5e13a138e72c300c
Your Name <you@example.com>
2025-01-05 06:15:14 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+  ],
+  darkMode: 'selector',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
